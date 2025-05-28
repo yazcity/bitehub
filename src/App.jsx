a621ca54203212a21096413613c92024626f1efd
yazcity <yaseenvk1990@gmail.com>
2025-05-28 20:00:06 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+// import Header from './Components/Header/Header'
+// import Welcome from './Pages/Welcome/Welcome'
+// import Footer from './Components/Footer/Footer'
+// import Partner from './Pages/Partner/Partner'
+// import Booking from './Pages/Booking/Booking'
+// import Dishes from './Pages/Dishes/Dishes'
+// import Ambience from './Pages/Ambience/Ambience'
+// import Delivery from './Pages/Delivery/Delivery'
+import LayoutRoutes from './Components/LayoutRoutes/LayoutRoutes'
+
+function App() {
+  return (
+    <>
+     <LayoutRoutes/>
+    </>
+  )
+}
+
+export default App
