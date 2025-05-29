02e8286e2354a5a0025cd132daee2f96fea2fc08
yazcity <yaseenvk1990@gmail.com>
2025-05-29 13:58:59 +0400

    fix-route-issue

--- src/Components/LayoutRoutes/LayoutRoutes.jsx
+++ src/Components/LayoutRoutes/LayoutRoutes.jsx
@@ -10,7 +10,7 @@ import NotFound from '../../Pages/NotFound/NotFound';
 
 function LayoutRoutes() {
   return (
-    <Router>
+    <Router basename="/bitehub">
         <Header/>
             <Routes>
             <Route path='/' element={ <Home/> }/>
